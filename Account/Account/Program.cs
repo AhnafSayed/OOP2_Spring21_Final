4a2af0f77225b4339a49a9739fb77dfdd264562f
Ahnaf Sayed <ahnafsayedtashri@gmail.com>
2021-03-21 11:58:26 +0600

    Bank account done!

---
+++ Account/Account/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Accounts acc1 = new Savings();
+            Accounts acc2 = new Savings("Hasan", 52606, 63000);
+            Accounts acc3 = new SpecialSavings("Jubaer", 57345, 45000, 20);
+
+            Accounts acc4 = new Fixed("Rahman", 52346, 6000);
+
+            Accounts acc5 = new Overdraft(1500);
+            SpecialSavings s1 = new SpecialSavings(30);
+
+            Overdraft od = new Overdraft("Hasan", 52606, 63000, 4500);
+        }
+    }
+}
